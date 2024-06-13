615bfc051eaceafd201f4eee81a288105a091e0c
Yousrry <yousry.arch@gmail.com>
2024-06-13 22:55:45 +0300

    hello

--- vars/SonarAnalysis.groovy
+++ vars/SonarAnalysis.groovy
@@ -3,7 +3,7 @@
 def call(){ 
 	withSonarQubeEnv(credentialsId: 'Sonarqube',installationName: 'Sonarqube' ) { 
 	
-		sh" ./gradlew sonarqube  -Dsonar.projectKey=SpringApp -Dsonar.host.url=http://35.174.205.184:9000  -Dsonar.login=sos"
+		sh"./gradlew sonarqube  -Dsonar.projectKey=SpringApp  -Dsonar.host.url=http://35.174.205.184:9000 -Dsonar.login=ab357f8cf0bcafdec79fbdb02743c5a7cd1ffa2c "
 	 
 	}
 }
